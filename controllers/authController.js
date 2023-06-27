2dc4ea09d8c47091015d9e71bbe39304d0a37add
Rick Hughey <hughey.thomas@gmail.com>
2023-06-27 06:25:18 -0500

    fixed emailing

--- controllers/authController.js
+++ controllers/authController.js
@@ -161,7 +161,7 @@ const forgotPassword = async (req, res) => {
   if (user) {
     const passwordToken = crypto.randomBytes(70).toString("hex");
     // send email
-    const origin = "http://localhost:5173";
+    const origin = "https://ideas-site.onrender.com";
     await sendResetPasswordEmail({
       name: user.name,
       email: user.email,
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,17 +1,40 @@
 import nodemailer from "nodemailer";
 import nodemailerConfig from "./nodemailerConfig.js";
+import sgMail from "@sendgrid/mail";
+
+// const sendEmailEthearal = async ({ to, subject, html }) => {
+//   let testAccount = await nodemailer.createTestAccount();
+
+//   const transporter = nodemailer.createTransport(nodemailerConfig);
+
+//   return transporter.sendMail({
+//     from: '"My guy" <foo@example.com>', // sender address
+//     to,
+//     subject,
+//     html,
+//   });
+// };
 
 const sendEmail = async ({ to, subject, html }) => {
-  let testAccount = await nodemailer.createTestAccount();
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const transporter = nodemailer.createTransport(nodemailerConfig);
-
-  return transporter.sendMail({
-    from: '"My guy" <foo@example.com>', // sender address
+  const msg = {
     to,
+    from: "rickbhughey@gmail.com", // Change to your verified sender
     subject,
+    text: "and easy to do anywhere, even with Node.js",
     html,
-  });
+  };
+  // const info = await sgMail.send(msg);
+  // res.json(info);
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 export default sendEmail;
